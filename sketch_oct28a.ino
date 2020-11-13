ab9b2355346a733e6165a05f887af8da29dfd8cb
Paige Plander <paigeplander@gmail.com>
2020-11-12 19:11:26 -0800

    Update sketch_oct28a.ino

--- sketch_oct28a.ino
+++ sketch_oct28a.ino
@@ -1,3 +1,10 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIXEL_PIN 9
+#define NUM_PIXELS 3
+Adafruit_NeoPixel npStrip(NUM_PIXELS, PIXEL_PIN);
+
+
 int motorEnablePinA = 11;
 int motorEnablePinB = 3;
 
@@ -16,26 +23,35 @@ long lecture_echo;
 long cm;
 long standardSpeed = 127;
 
-long barrierCm = 100;
+long barrierCm = 90;
+
+unsigned long timeSinceDance = millis();
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // motors
   pinMode(motorEnablePinA, OUTPUT);
   pinMode(motorEnablePinB, OUTPUT);
   pinMode(motorPinA1, OUTPUT);
   pinMode(motorPinA2, OUTPUT);
+
+  // sensors
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
 
   // why?
   digitalWrite(trigPin, LOW);
 
+
+  /// neopixel
+  npStrip.begin(); 
+
   Serial.begin(9600);
-  
 }
 
 void loop() {  
   // get distance 
+  
   digitalWrite(trigPin, HIGH); 
   delayMicroseconds(10); 
   digitalWrite(trigPin, LOW); 
@@ -43,10 +59,11 @@ void loop() {
   cm = lecture_echo / 58; 
 
   if (cm < barrierCm) {
+   setStopPixels();
    // stop
    objInWay = true;
    stopMoving();
-   delay(1000);
+   delay(100);
    // back up 
    goBackwards(standardSpeed);
    delay(500);
@@ -60,6 +77,7 @@ void loop() {
   }
   
   else {
+    setForwardPixels();
     objInWay = false;
     shouldTurnLeft = !shouldTurnLeft;
     goForward(standardSpeed);
@@ -67,6 +85,59 @@ void loop() {
   }
 }
 
+
+void setForwardPixels() {
+  //npStrip.clear();
+  //npStrip.show();
+  for (int i=0; i<NUM_PIXELS; i++) {
+    npStrip.setPixelColor(i, random(0, 255), random(0, 255), random(0, 255));
+    delay(100);
+    npStrip.show();
+  }
+  for (int i=NUM_PIXELS-1; i>=0; i--) {
+    npStrip.setPixelColor(i, random(0, 255), random(0, 255), random(0, 255));
+    delay(100);
+    npStrip.show();
+  }
+}
+
+void setStopPixels() {
+  npStrip.clear();
+  for (int i=0; i<NUM_PIXELS; i++) {
+    npStrip.setPixelColor(i, 255, 0, 0);
+  }
+  npStrip.show();
+}
+
+
+//void rainbow(uint8_t wait) {
+//  uint16_t i, j;
+//
+//  for(j=0; j<256; j++) {
+//    for(i=0; i<NUM_PIXELS; i++) {
+//      npStrip.setPixelColor(i, Wheel((i*1+j) & 255));
+//    }
+//    npStrip.show();
+//    delay(wait);
+//  }
+//}
+//
+//
+//// Input a value 0 to 255 to get a color value.
+//// The colours are a transition r - g - b - back to r.
+//uint32_t Wheel(byte WheelPos) {
+//  if(WheelPos < 85) {
+//    return npStrip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+//  } 
+//  else if(WheelPos < 170) {
+//    WheelPos -= 85;
+//    return npStrip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+//  } 
+//  else {
+//    WheelPos -= 170;
+//    return npStrip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+//  }
+//}
 
 void goForward(long vel) {
   analogWrite(motorEnablePinA, vel);
